054380bcfb0183e62e5c23fff1734827d4ffa06d
Moses Kivua <kivuamoses9@gmail.com>
2022-08-21 23:27:19 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  if(feet<=400){
+    return 'This one is on me!'
 }
-
-function ternaryCheckCity(){
-  // Write your code here!
+else if(feet>2000 && feet<2500){
+  return 'I will gladly take your thirty bucks.'
 }
-
-function switchOnCharmFromTip(){
+else {
+  return 'No can do.'
+}
+}
+function ternaryCheckCity (city){
+  // Write your code here! 
+return city==='NYC' ? "Ok, sounds good.":"No go."
+}
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  if (tip==='generous'){
+    return "Thank you so much."
+  }
+  else if(tip==='not as generous'){
+    return "Thank you."
+  }
+  else{
+    return "Bye."
+  }
 }
